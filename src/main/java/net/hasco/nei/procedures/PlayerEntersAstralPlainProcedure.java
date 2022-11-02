f126cfa3b7e24b1e247aee394c189a1dbc50d06d
Ace <77860646+AceSardonyx@users.noreply.github.com>
2022-11-02 10:40:32 -0400

    Fixed astral plains advancement without test
    YEAH I DIDNT TEST IT OK

--- src/main/java/net/hasco/nei/procedures/PlayerEntersAstralPlainProcedure.java
+++ src/main/java/net/hasco/nei/procedures/PlayerEntersAstralPlainProcedure.java
@@ -6,10 +6,9 @@ import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
 
 import net.minecraft.world.entity.Entity;
-import net.minecraft.tags.TagKey;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.core.Registry;
 import net.minecraft.advancements.AdvancementProgress;
 import net.minecraft.advancements.Advancement;
 
@@ -31,7 +30,10 @@ public class PlayerEntersAstralPlainProcedure {
 	private static void execute(@Nullable Event event, Entity entity) {
 		if (entity == null)
 			return;
-		if (entity.getType().is(TagKey.create(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("nei:awoken")))) {
+		if (entity instanceof ServerPlayer _plr && _plr.level instanceof ServerLevel
+				? _plr.getAdvancements().getOrStartProgress(_plr.server.getAdvancements().getAdvancement(new ResourceLocation("nei:into_the_sky")))
+						.isDone()
+				: false) {
 			if (entity instanceof ServerPlayer _player) {
 				Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("nei:a_little_place_called_home"));
 				AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
